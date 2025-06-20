1fb91e8a62404ef11cf7d43c389127892bddc769
nagarajulnr232 <nagarajulnr232@gmail.com>
2025-06-20 14:48:45 +0530

    Added

---
+++ 003_fizzBuzz.py
@@ -0,0 +1,11 @@
+max_num=int(input("Enter number : "))
+
+for num in range(1, max_num+1):
+    if num % 3 == 0 and num % 5 == 0:
+        print('FizzBuzz')
+    elif num % 3 == 0:
+        print('Fizz')
+    elif num % 5 == 0:
+        print('Buzz')
+    else:
+        print(num)
